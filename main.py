5d358d7c8e0572f27ae53462e1ace0619d0956a8
dzy <1129881228@qq.com>
2025-03-26 03:55:49 +0800

    init

---
+++ main.py
@@ -0,0 +1,25 @@
+from websocket.server import WebSocketServer
+from websocket.router import WebSocketRouter, Blueprint
+
+
+def create_app():
+    router = WebSocketRouter()
+
+    # 自动注册所有蓝图
+    Blueprint.auto_register(
+        router,
+        package_path='blueprints',
+        bp_suffix='_bp'
+    )
+
+    return WebSocketServer(("0.0.0.0", 9000), router)
+
+
+if __name__ == "__main__":
+    server = create_app()
+    print("WebSocket server running on ws://localhost:9000")
+    try:
+        server.serve_forever()  # 启动服务
+    except KeyboardInterrupt:
+        server.shutdown()  # 优雅关闭
+        print("\n🛑 Server stopped")
